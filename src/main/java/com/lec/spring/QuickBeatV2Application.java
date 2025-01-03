c76a489a87a3c39aa89a5604309bb42883535554
Donghee Lee <dhleedev47@gmail.com>
2025-01-03 14:49:52 +0900

    Initial commit

---
+++ src/main/java/com/lec/spring/QuickBeatV2Application.java
@@ -0,0 +1,13 @@
+package com.lec.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickBeatV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickBeatV2Application.class, args);
+    }
+
+}
