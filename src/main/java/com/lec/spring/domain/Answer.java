2df88f601319ebbdf961236c2a048226f5594506
Donghee Lee <dhleedev47@gmail.com>
2025-01-14 10:10:57 +0900

    Feat(): qr data 불러오기 완료

--- src/main/java/com/lec/spring/domain/Answer.java
+++ src/main/java/com/lec/spring/domain/Answer.java
@@ -2,14 +2,12 @@ package com.lec.spring.domain;
 
 import lombok.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Answer {
+public class Playlist{
     private String id;
-    private LocalDateTime createdAt;
     private List<Track> tracks;
 }
